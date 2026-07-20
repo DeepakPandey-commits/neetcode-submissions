9ec67b26950f6faaa9c32e75933a4ce482ecd3c1
Deepak Pandey <Deepakpandeyy94@gmail.com>
2026-07-20 20:40:38 +0530

    Add: custom-sort-string - submission-0

---
+++ Data Structures & Algorithms/custom-sort-string/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String customSortString(String order, String s) {
+        int[] freq = new int[26];
+
+        // Count frequency of characters in s
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        // Add characters according to custom order
+        for (int i = 0; i < order.length(); i++) {
+            char ch = order.charAt(i);
+
+            while (freq[ch - 'a'] > 0) {
+                ans.append(ch);
+                freq[ch - 'a']--;
+            }
+        }
+
+        // Add remaining characters not present in order
+        for (int i = 0; i < 26; i++) {
+            while (freq[i] > 0) {
+                ans.append((char) (i + 'a'));
+                freq[i]--;
+            }
+        }
+
+        return ans.toString();
+    }
+}
